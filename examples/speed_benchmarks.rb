3864e8bb183ffc69c22305537409b9cd75822dd0
jfcalvo <josefranciscocalvo@gmail.com>
2011-09-16 17:53:05 +0200

    Very big speed improvement.

--- examples/speed_benchmarks.rb
+++ examples/speed_benchmarks.rb
@@ -8,7 +8,7 @@ def number_of_ones(chromosome)
 end
 
 def distance(chromosome)
-  levenshtein_distance = Text::Levenshtein.distance("genetica", chromosome.chromosome.join.strip)
+  levenshtein_distance = Text::Levenshtein.distance("genetica", chromosome.chromosome.join)
   if levenshtein_distance == 0
     return 2
   else
@@ -90,13 +90,14 @@ class SpeedBenchmark
     @population_builder.crossover_probability = 0.7                 # Crossover rate
     @population_builder.mutation_probability = 0.001                # Mutation rate
     @population_builder.chromosome_length = 8                       # Chromosome length
-    @population_builder.chromosome_alleles = ('a'..'z').to_a << ' ' # Chromosome alleles
+    @population_builder.chromosome_alleles = ('a'..'z').to_a        # Chromosome alleles
     @population_builder.fitness_function = method(:distance)        # Fitness Function
 
     population = @population_builder.population
 
     while population.best_fitness < 2
       population.run generations=1
+      puts population.best_chromosome
     end
   end
 
--- lib/genetica/population.rb
+++ lib/genetica/population.rb
@@ -10,39 +10,46 @@ module Genetica
     attr_accessor :fitness_function
 
     def initialize(population)
-      @population = population
       @generation = 0
+      @population = population
     end
 
     def best_chromosome
-      @population.at self.population_fitness.index(self.best_fitness)
+      @population.at @population_fitness.index(self.best_fitness)
     end
 
     def best_fitness
-      self.population_fitness.max      
+      @population_fitness.max
     end
 
     def average_fitness
-      self.population_fitness.inject(:+) / population_fitness.size.to_f
+      @population_fitness.inject(:+) / @population_fitness.size.to_f
     end
 
     def population_fitness
       @population.collect { |chromosome| @fitness_function.call(chromosome) }
     end
-    
-    def fitness_proportionate_selection
-      # Get an array with chromosome fitness      
-      population_fitness = self.population_fitness
 
+    def fitness_function=(new_fitness_function)
+      @fitness_function = new_fitness_function
+      @population_fitness = self.population_fitness
+    end
+
+    def population=(new_population)
+      @population = new_population
+      @population_fitness = self.population_fitness
+    end
+
+    def fitness_proportionate_selection
       # FUTURE: With Ruby 1.9.3 you can use rand with ranges, e.g. rand 0.0..3.4
       # Get random number
       random_generator = Random.new
-      random_number = random_generator.rand 0.0..population_fitness.inject(:+)
+      random_number = random_generator.rand 0.0..@population_fitness.inject(:+)
 
       # Chromosome selection
       fitness_counter = 0
       @population.each_with_index do |chromosome, i|
-        fitness_counter += population_fitness[i]
+        fitness_counter += @population_fitness[i]
         if fitness_counter >= random_number
           return chromosome
         end
@@ -83,7 +90,7 @@ module Genetica
         population.delete_at rand population.size if @population.size.odd?
 
         # Replacing chromosome population with the new one
-        @population = population
+        self.population = population
 
         # A new generation has been created
         @generation += 1
